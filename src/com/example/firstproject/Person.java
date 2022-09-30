efe0e940774feca5b911f7d2b1f232cd7616ec37
George Asmaro <george.asmaro@hotmail.com>
2022-09-30 15:03:53 -0700

    ....

---
+++ src/com/example/firstproject/Person.java
@@ -0,0 +1,53 @@
+package com.example.firstproject;
+
+import java.util.Scanner;
+
+public class Person {
+
+    Scanner input = new Scanner(System.in);
+    int age;
+    String name;
+    String quote;
+
+    public Person(int yourAge, String yourName, String favQuote) {
+        age = yourAge;
+        name = yourName;
+        quote = favQuote;
+    }
+
+    public int getAge() {
+        System.out.println(age);
+        return age;
+    }
+
+    public int setAge() {
+        System.out.println("Enter the age you would like to set");
+        age = input.nextInt();
+        System.out.println("Age is set to: " + age + " years old");
+        return age;
+    }
+
+    public String getName() {
+        System.out.println(name);
+        return name;
+    }
+
+    public String setName() {
+        System.out.println("Enter the age you would like to set");
+        name = input.nextLine();
+        System.out.println("Name is set to: " + name);
+        return name;
+    }
+
+    public String getFavQuote() {
+        System.out.println(quote);
+        return quote;
+    }
+
+    public String setFavQuote() {
+        System.out.println("Enter the quote you would like to set");
+        quote = input.nextLine();
+        System.out.println("Favourite quote is set to: " + quote);
+        return quote;
+    }
+}
