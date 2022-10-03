6198e1663dcb3ec7071c29e6e185e74de6ed37e8
George Asmaro <g.asmaro@surreyschools.ca>
2022-10-03 09:27:29 -0700

    Adding comments to the person class

--- src/com/example/firstproject/Person.java
+++ src/com/example/firstproject/Person.java
@@ -9,17 +9,19 @@ public class Person {
     String name;
     String quote;
 
+    //Creates a constructor for the person class requiring an age, name and favourite quote
     public Person(int yourAge, String yourName, String favQuote) {
         age = yourAge;
         name = yourName;
         quote = favQuote;
     }
-
+    //A getter for the age which prints the age given by the user
     public int getAge() {
         System.out.println("My age is: " + age);
         return age;
     }
 
+    //A setter for the age which prints the age set by the user
     public int setAge() {
         System.out.println("Enter the age you would like to set");
         age = input.nextInt();
@@ -27,11 +29,13 @@ public class Person {
         return age;
     }
 
+    //A getter for the name which prints the name given by the user
     public String getName() {
         System.out.println(name);
         return name;
     }
 
+    //A setter for the name which prints the name set by the user
     public String setName() {
         System.out.println("Enter the name you would like to set");
         name = input.nextLine();
@@ -39,11 +43,13 @@ public class Person {
         return name;
     }
 
+    //A getter for the quote which prints the quote given by the user
     public String getFavQuote() {
         System.out.println(quote);
         return quote;
     }
 
+    //A setter for the quote which prints the quote set by the user
     public String setFavQuote() {
         System.out.println("Enter the quote you would like to set");
         quote = input.nextLine();
@@ -51,6 +57,7 @@ public class Person {
         return quote;
     }
 
+    //A getter for all the variables printing all the information given by the user
     public void getAll() {
         System.out.println("My name is " + name + " and I am " + age + " years old." );
         System.out.println("My favourite quote is: " + quote);
